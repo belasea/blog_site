b597cee32d9cfcd8ca4fcbdacce557bef57d7308
jibontech <jibon.belaface@gmail.com>
2025-08-04 15:39:55 +0600

    added some file

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party library
+    'import_export',
 ]
 
 # Custom User Model
